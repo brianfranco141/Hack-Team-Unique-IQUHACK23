a36b551c472916541b552454b05dc3950f2f06fe
mohadesehazari98 <84409349+mohadesehazari98@users.noreply.github.com>
2023-01-29 09:52:13 -0500

    Add files via upload

---
+++ Distance.ino
@@ -0,0 +1,220 @@
+int temppin=0;
+
+float temp;
+
+void setup()
+
+{
+
+Serial.begin(9600);
+
+}
+
+void loop()
+
+{
+
+temp=analogRead(temppin); // Reading data from the sensor.This voltage is stored as a 10bit number
+
+temp=(5.0*temp*1000.0)/(1024*10);
+
+/* 5*temp/1024 is to convert the 10 bit number to a voltage reading.
+
+This is multiplied by 1000 to convert it to millivolt.
+
+We then divide it by 10 beacuse each degree rise results in a 10 millivolt increase.
+
+*/
+
+Serial.println(temp);
+
+delay(500);// This is because we dont want a continuous stream of data
+
+}
+/* now we have to transfer data from arduino to the things speak,Mohadeseh.......................................................................................................
+ */
+
+
+ 
+ #include <SPI.h>
+#include <WiFi101.h>
+
+
+// Local Network Settings
+#include "arduino_secrets.h" 
+///////please enter your sensitive data in the Secret tab/arduino_secrets.h
+char ssid[] = SECRET_SSID;        // your network SSID (name)
+char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
+int keyIndex = 0;             // your network key Index number (needed only for WEP)
+
+int status = WL_IDLE_STATUS;
+
+WiFiServer server(80);
+
+// ThingSpeak Settings
+char thingSpeakAddress[] = "api.thingspeak.com";
+String APIKey = "XXXXXXXXXXXXXXXX";             // enter your channel's Write API Key
+const int updateThingSpeakInterval = 20 * 1000; // 20 second interval at which to update ThingSpeak
+
+// Variable Setup
+long lastConnectionTime = 0;
+boolean lastConnected = false;
+
+// Initialize Arduino Ethernet Client
+WiFiClient client;
+
+void setup() {
+  // Start Serial for debugging on the Serial Monitor
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }
+
+  // check for the presence of the shield:
+
+  /*this if statement waits for that dalay and it checks wether the client is still connected or not*/
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present");
+    // don't continue:
+    while (true);
+  }
+
+  // attempt to connect to Wifi network:........................................................................................................................................
+  while ( status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+    status = WiFi.begin(ssid, pass);
+
+    // wait 10 seconds for connection:
+    delay(10000);
+  }
+  // you're connected now, so print out the status:
+  printWifiStatus();
+}
+
+void loop() {
+  // read values from pins and store as strings
+
+
+  String light = String(analogRead(A0), DEC); // read light value
+
+  // find temp value
+  float voltage = analogRead(A1) * (3.3 / 1024);  // convert 0-1023 range to 3.3V range
+  int tempVal = (voltage - 0.5) * 100;            // convert voltage to temperature in *C
+  String temp = String(tempVal);
+
+  // Print Update Response to Serial Monitor
+  if (client.available()) {
+    char c = client.read();
+    Serial.print(c);
+  }
+  // Disconnect from ThingSpeak
+  if (!client.connected() && lastConnected) {
+    Serial.println("...disconnected");
+    Serial.println();
+    client.stop();
+  }
+  // Update ThingSpeak
+  if (!client.connected() && (millis() - lastConnectionTime > updateThingSpeakInterval)) {
+    updateThingSpeak("field1=" + light + "&field2=" + temp);
+    Serial.println(light);
+    Serial.println(temp);
+
+  }
+  lastConnected = client.connected();
+}
+
+void updateThingSpeak(String tsData) {
+  if (client.connect(thingSpeakAddress, 80)) {
+    client.print("POST /update HTTP/1.1\n");
+    client.print("Host: api.thingspeak.com\n");
+    client.print("Connection: close\n");
+    client.print("X-THINGSPEAKAPIKEY: " + APIKey + "\n");
+    client.print("Content-Type: application/x-www-form-urlencoded\n");
+    client.print("Content-Length: ");
+    client.print(tsData.length());
+    client.print("\n\n");
+    client.print(tsData);
+    lastConnectionTime = millis();
+
+    if (client.connected()) {
+      Serial.println("Connecting to ThingSpeak...");
+      Serial.println();
+    }
+  }
+}
+
+void printWifiStatus() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your WiFi shield's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+}
+
+/* calculating the number of people,Mohadeseh ...................................................................................................................
+ */
+
+
+ /*
+ * created by Rui Santos, https://randomnerdtutorials.com
+ * 
+ * Complete Guide for Ultrasonic Sensor HC-SR04
+ *
+    Ultrasonic sensor Pins:
+        VCC: +5VDC
+        Trig : Trigger (INPUT) - Pin11
+        Echo: Echo (OUTPUT) - Pin 12
+        GND: GND
+ */
+ 
+int trigPin = 11;    // Trigger
+int echoPin = 12;    // Echo
+long duration, cm, inches;
+ 
+void setup() {
+  //Serial Port begin
+  Serial.begin (9600);
+  //Define inputs and outputs
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+ 
+void loop() {
+  // The sensor is triggered by a HIGH pulse of 10 or more microseconds.
+  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(5);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+ 
+  // Read the signal from the sensor: a HIGH pulse whose
+  // duration is the time (in microseconds) from the sending
+  // of the ping to the reception of its echo off of an object.
+  pinMode(echoPin, INPUT);
+  duration = pulseIn(echoPin, HIGH);
+ 
+  // Convert the time into a distance
+  cm = (duration/2) / 29.1;     // Divide by 29.1 or multiply by 0.0343
+  inches = (duration/2) / 74;   // Divide by 74 or multiply by 0.0135
+  
+  Serial.print(inches);
+  Serial.print("in, ");
+  Serial.print(cm);
+  Serial.print("cm");
+  Serial.println();
+  
+  delay(250);
+}
+/*.....................................................................................................................................
